7285ec7599904620f013c897459522ef4d4a3396
a_Dong <a_Dong@sjtu.edu.cn>
2021-07-22 13:59:20 +0800

    last-update of greedy

--- agent/greedy/submission.py
+++ agent/greedy/submission.py
@@ -1,6 +1,9 @@
 import copy
 import math
 import numpy as np
+from numpy.lib import stride_tricks
+from torch._C import dtype
+
 def get_id(x, y, width):
     return x * width + y
     
@@ -85,7 +88,34 @@ def diji(state, X, Y, width, height):
                 pq.put((mp[x1][y1],(x1,y1)))
     return mp
 
+def keep_safe(X, Y, turn, state, width, height, snakes):
+    vis=np.zeros((height,width))
+    pq=[]
+    pq.put((X,Y))
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+    mx = snakes[turn][-1][0]
+    my = snakes[turn][-1][1]
+    cnt = 0
+    while (not pq.empty()):
+        (x,y) =pq.get()
+        if (vis[x][y]==1): continue
+        vis[x][y] = 1
+        cnt += 1
+        for i in range(4):
+            x1=x+dx[i]
+            y1=y+dy[i]
+            x1 += height
+            x1 %= height
+            y1 += width
+            y1 %= width
+            
+            if (state[x1][y1]==2 or state[x1][y1]==3 and (x1 != mx and y1 != my)): continue
+            pq.put((x1,y1))
+    return cnt
+
 def get_min_bean(x, y, beans_position, width, height, snakes, state):
+    if (len(beans_position)==0): return 0,0,-1
     min_distance = math.inf
     min_x = beans_position[0][1]
     min_y = beans_position[0][0]
@@ -171,10 +201,11 @@ def keep_safe(X, Y, turn, state, width, height, snakes):
             x1 %= height
             y1 += width
             y1 %= width
+            
             if (state[x1][y1]==2 or state[x1][y1]==3 and (x1 != mx or y1 != my)): continue
             pq.put((x1,y1))
     return cnt
-def get_map(state, beans, snakes, width, height, turn, dir):
+def get_map(state, snakes, width, height, turn, dir):
     mp2=state.copy()
     x= snakes[turn][0][0]
     y = snakes[turn][0][1]
@@ -195,7 +226,7 @@ def get_map(state, beans, snakes, width, height, turn, dir):
     return mp2
 
 def Cnt_d(state, beans, snakes, width, height, turn, dir):
-    mp=get_map(state,beans,snakes,width,height,turn,dir)
+    mp=get_map(state,snakes,width,height,turn,dir)
     dx = [-1,1,0,0]
     dy = [0,0,-1,1]
     d=np.zeros((height,width))
@@ -234,9 +265,11 @@ def Check_Circle(snakes, id, width, height):
         if (x1==Lx and y1 == Ly): return (True,i)
     return (False,-1)
 
+
 def bfs(state, target, snakes, width, height, turn):
     from queue import Queue
     # print(target,"target")
+    leng = len(target)
     Q = Queue()
     Q.put((-1,snakes[turn][0][0],snakes[turn][0][1],-1))  # step, x,y, fir_dir
     dx = [-1,1,0,0]
@@ -244,7 +277,7 @@ def bfs(state, target, snakes, width, height, turn):
     while (not Q.empty()):
         (step, x,y , dir) = Q.get()
         # print(step,x,y,dir,"(step,x,y,dir)")
-        if (step > 8): return (-1,-1)
+        if (step > 9): return (-1,-1)
         for i in range(4):
             x1 = x + dx[i]
             y1 = y + dy[i]
@@ -253,7 +286,7 @@ def bfs(state, target, snakes, width, height, turn):
             x1 %= height
             y1 %= width
             # print(x1,y1,"x1,y1")
-            if (x1==target[step+1][0] and y1 == target[step+1][1]):
+            if (x1==target[(step+1)%leng][0] and y1 == target[(step+1)%leng][1]):
                 # print(x1,y1)
                 if (step==-1): return (i, step+1)
                 else: return (dir, step+1)
@@ -263,6 +296,7 @@ def bfs(state, target, snakes, width, height, turn):
             else:
                 Q.put((step+1,x1,y1,dir))
     return (-1,-1)
+    
 def Get_NEW_MAP(state, Lx, Ly, snakes, id, height, width):
     mp2= copy.deepcopy(state)
     mp2[Lx][Ly]=0
@@ -281,6 +315,7 @@ def Get_NEW_MAP(state, Lx, Ly, snakes, id, height, width):
         if (state[x1][y1]==1): tmp+=1
     if (tmp==0): mp2[snakes[id][-1][0]][snakes[id][-1][1]]=0
     return mp2
+
 def f(delta_len, my_len, d_bean, d_rear):
     if (delta_len<=0 or my_len<=11): return d_bean
     if (delta_len==1 or my_len<=12): return d_bean*0.9+d_rear*0.1
@@ -289,6 +324,85 @@ def f(delta_len, my_len, d_bean, d_rear):
     if (delta_len<=6 or my_len<=17): return d_bean*0.3+d_rear*0.7
     if (delta_len<=9 or my_len<=19): return d_bean*0.1+d_rear*0.9
     return d_rear
+
+#return val,dir
+
+def get_snakes(state,Snakes,width,height,turn, dir):
+    snakes = copy.deepcopy(Snakes)
+    x = snakes[turn][0][0]
+    y = snakes[turn][0][1]
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+    x += dx[dir]
+    y += dy[dir]
+    x += height
+    x %= height
+    y += width
+    y %= width
+    snakes[turn].insert(0,[x,y])
+    if (state[x][y]!=1): snakes[turn].pop()
+    return snakes
+
+def dfs(state, snakes, width, height, turn, step):
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+    from queue import Queue
+    Q=Queue()
+    Q.put((state,snakes,0,0,-1))  #state, snakes, steps, val,dir
+    ans_dir=0
+    ans_val=-1
+    # print("step",step)
+    while (not Q.empty()):
+        (mp,S,st,val,dir)= Q.get()
+        x = S[turn][0][0]
+        y = S[turn][0][1]
+        Lx = S[turn][-1][0]
+        Ly = S[turn][-1][1]
+        if (st==step): break
+        for i in range(4):
+            xx = x + dx[i]
+            yy = y + dy[i]
+            xx += height
+            yy += width
+            xx %= height
+            yy %= width
+            if (mp[xx][yy]<=1 or xx==Lx and yy==Ly):
+                nowval = val
+                if (mp[xx][yy]==1): nowval+=1
+                thisdir= dir
+                if (dir==-1): thisdir=i
+                if (nowval>=ans_val):
+                    ans_val = nowval
+                    ans_dir = thisdir
+                Q.put((get_map(mp,S,width,height,turn,i), get_snakes(mp,S,width,height,turn,i),st+1,nowval,thisdir))
+    return (ans_val,ans_dir)
+
+def Hit_the_wall (state, snakes, width, height, steps, turn):
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+    from queue import Queue
+    Q = Queue()
+    Q.put((state, snakes, -1,0)) # state snakes dir step
+    while (not Q.empty()):
+        (mp, S, dir, st) = Q.get()
+        if (st>=steps): break
+        x = S[turn][0][0]
+        y = S[turn][0][1]
+        for i in range(4):
+            xx = x + dx[i]
+            yy = y + dy[i]
+            xx += height
+            yy += width
+            xx %= height
+            yy %= width
+            if (xx == S[turn][1][0] and yy == S[turn][1][1]): continue
+            if (mp[xx][yy]>1 and (xx != S[turn][-1][0] or yy != S[turn][-1][1])):
+                if (dir == -1): return i
+                else: return dir
+            thisdir=dir
+            if (dir==-1): thisdir = i
+            Q.put((get_map(mp,S,width,height,turn,i),get_snakes(mp,S,width,height,turn,i),thisdir, st+1))
+    return -1
 
 def greedy_snake(state_map, beans, snakes, width, height, ctrl_agent_index, Current_Step):
     beans_position = copy.deepcopy(beans)
@@ -308,12 +422,22 @@ def greedy_snake(state_map, beans, snakes, width, height, ctrl_agent_index, Curr
 
         if (len_my < len_U-2 and (len_U >= 14 or len_U-len_my>=5)):
             (Flag, dirt) = Check_Circle(snakes,i^1,width,height) 
-            if (Flag and (snakes[i^1][0][0]-snakes[i][0][0]+snakes[i^1][0][1]-snakes[i][0][1])%2==0):
-                (dir,NEED_step) = bfs(state_map, snakes[i^1][-1:-10:-1],snakes,width,height,i)
-                # print(666)
-                # print(dir,NEED_step)
-                # print()
-                if (dir != -1 and NEED_step < 50-Current_Step): 
+            if (Flag):
+                if ((snakes[i^1][0][0]-snakes[i][0][0]+snakes[i^1][0][1]-snakes[i][0][1])%2==0):
+                    (dir,NEED_step) = bfs(state_map, snakes[i^1][-1::-1],snakes,width,height,i)
+                    # print(666)
+                    # print(dir,NEED_step)
+                    # print()
+                    if (dir != -1 and NEED_step <= 50-Current_Step+1): 
+                        actions.append(dir)
+                        return actions
+                if (len_U-len_my>=8):
+                    dir = Hit_the_wall (state_map, snakes, width, height, 50-Current_Step+1, i)
+                    if (dir!=-1):
+                        actions.append(dir)
+                        return actions
+                (val, dir) = dfs(state_map,snakes,width,height,i,min((50-Current_Step+1),8))
+                if (val>=2):
                     actions.append(dir)
                     return actions
         Map_without_my_rear = Get_NEW_MAP(state_map, Lx, Ly,snakes, i^1,height, width)
@@ -343,7 +467,7 @@ def greedy_snake(state_map, beans, snakes, width, height, ctrl_agent_index, Curr
                 T[j]=100000
             else: 
                 Blok[j]= keep_safe(head_x_tmp, head_y_tmp,ctrl_agent_index[0],Map_without_my_rear,width,height,snakes)
-                T[j] = keep_safe(snakes[i^1][0][0],snakes[i^1][0][1],i^1,get_map(state_map,beans,snakes,width,height,i,j),width,height,snakes)
+                T[j] = keep_safe(snakes[i^1][0][0],snakes[i^1][0][1],i^1,get_map(state_map,snakes,width,height,i,j),width,height,snakes)
                 if (Blok[j]>T[j]+2 and T[j]<=min(5,50-Current_Step)):
                     Blok[j]=66
                 else:
@@ -351,6 +475,17 @@ def greedy_snake(state_map, beans, snakes, width, height, ctrl_agent_index, Curr
                     else: Blok[j]=55
                     if (head_x_tmp == Lx and head_y_tmp == Ly): Blok[j]=55
                     if (head_x_tmp == snakes[i^1][-1][0] and head_y_tmp == snakes[i^1][-1][1]): Blok[j]=0
+            if (len_my>len_U+1):
+                for k in range(4):
+                    xx = snakes[i^1][0][0] + dx[k]
+                    yy = snakes[i^1][0][1] + dy[k]
+                    xx += height
+                    xx %= height
+                    yy += width
+                    yy %= width
+                    if (xx==head_x_tmp and yy==head_y_tmp):
+                        Blok[j] -= 10
+                        break
             if (head_surrounding[j]>1): D[j]=-10000
             else: D[j]= Cnt_d(state_map,beans,snakes,width,height,ctrl_agent_index[0],j)
             Tup.append((Blok[j],-dis[j],D[j],j))
@@ -366,23 +501,7 @@ def to_joint_action(actions, num_agent):
         one_hot_action = [one_hot_action]
         joint_action.append(one_hot_action)
     return joint_action
-
-
-current_step2=0
-# from functools import wraps
-# current_step=0
-# def wrapper(func):  # func = my_controller
-#     @wraps(func)
-#     def inner(*args, **kwargs):
-#                                          #step1
-#         ret = func(*args, **kwargs)
-#         global current_step
-#         current_step+=1
-#         print(current_step)                           #step3
-#         return ret
-#     return inner
-
-# @wrapper   # my_controller = wrapper(my_controller)
+current_step2 = 0
 def my_controller(observation_list, action_space_list, is_act_continuous=False):
     global current_step2
     current_step2 += 1
