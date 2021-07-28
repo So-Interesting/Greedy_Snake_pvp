f9c7f35ad6b63679ff83aec832911aeff926fef2
a_Dong <a_Dong@sjtu.edu.cn>
2021-07-29 01:14:23 +0800

    last update for greedy

--- agent/greedy/greedy_agent.py
+++ agent/greedy/greedy_agent.py
@@ -500,7 +500,6 @@ def greedy_snake(state_map, beans, snakes, width, height, ctrl_agent_index, Curr
         actions.append(Tup.index(max(Tup))) 
     return actions
 
-
 def to_joint_action(actions, num_agent):
     joint_action = []
     for i in range(num_agent):
